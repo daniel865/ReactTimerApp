fff7a0131fde09cd71cfae948cdd13b43fc7f902
daniel <daniel865live@gmail.com>
2016-10-15 17:07:22 -0500

    Add Clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Clock from './Clock';
 
 export default class Countdown extends React.Component {
   constructor(props) {
@@ -7,7 +8,9 @@ export default class Countdown extends React.Component {
 
   render() {
     return (
-      <div>Countdown</div>
+      <div>
+        <Clock totalSeconds={129} />
+      </div>
     );
   }
 }
---
+++ app/tests/components/Clock.test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import TestUtils from 'react-addons-test-utils';
+var $ = require('jQuery');
+
+
+import Clock from '../../components/Clock';
+
+describe('Clock', () => {
+  it('should exist', () => {
+    expect(Clock).toExist();
+  });
+
+  describe('render', () => {
+    it('should render clock to output', () => {
+      let clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62} />);
+      let $el =  $(ReactDOM.findDOMNode(clock));
+      let actualText = $el.find('.clock-text').text();
+
+      expect(actualText).toBe('01:02');
+    });
+  })
+
+  describe('formatSeconds', () => {
+    it('should format seconds', () => {
+      let clock = TestUtils.renderIntoDocument(<Clock />);
+      let seconds = 615;
+      let expected = '10:15';
+      let actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
+
+    it('should format seconds when min/sec are less than 10', () => {
+      let clock = TestUtils.renderIntoDocument(<Clock />);
+      let seconds = 615;
+      let expected = '10:15';
+      let actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
+  });
+})
