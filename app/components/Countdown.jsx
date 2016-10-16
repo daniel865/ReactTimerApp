10ae6b143fbbf5588c341629684df2a4311b69b3
daniel <daniel865live@gmail.com>
2016-10-15 22:55:12 -0500

    Add CountdownForm and tests

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import Clock from './Clock';
+import CountdownForm from './CountdownForm';
 
 export default class Countdown extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0
+    }
+
+    this.handleSetCountdown = this.handleSetCountdown.bind(this);
   }
 
-  render() {
+  handleSetCountdown(seconds){
+    this.setState({
+      count: seconds
+    });
+  }
+
+  render(){
+    let {count} = this.state;
+
     return (
       <div>
-        <Clock totalSeconds={129} />
+        <Clock totalSeconds={count} />
+        <CountdownForm onSetCountdown={this.handleSetCountdown} />
       </div>
     );
   }
---
+++ app/tests/components/CountdownForm.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import TestUtils from 'react-addons-test-utils';
+var $ = require('jQuery');
+
+import CountdownForm from '../../components/CountdownForm';
+
+describe('CountdownForm', () => {
+  it('should exist', () => {
+    expect(CountdownForm).toExist();
+  });
+
+  it('should call onSetCountdown if valid seconds entered', () => {
+    let spy = expect.createSpy();
+    let countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy} />);
+    let $el = $(ReactDOM.findDOMNode(countdownForm));
+
+    countdownForm.refs.seconds.value = '109';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith(109);
+  });
+
+  it('should not call onSetCountdown if invalid seconds entered', () => {
+    let spy = expect.createSpy();
+    let countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy} />);
+    let $el = $(ReactDOM.findDOMNode(countdownForm));
+
+    countdownForm.refs.seconds.value = 'a109';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toNotHaveBeenCalled();
+  });
+
+})
